d5b434918443ff036d31906c5c4e189ae4ef839b
Matthias Seyock <matthias.seyock@prodot.de>
2019-07-23 16:06:29 +0200

    #9 Implement exception handling

--- JiraToolkit/JiraToolkit/App.xaml.cs
+++ JiraToolkit/JiraToolkit/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows;
+using System.Windows.Threading;
+using log4net;
 using Shuriken.Monitoring;
 
 namespace JiraToolkit
@@ -9,10 +12,13 @@ namespace JiraToolkit
     /// </summary>
     public partial class App : Application
     {
+        static readonly ILog logger = LogManager.GetLogger(typeof(Application));
+
         [STAThread]
         public static void Main()
         {
             var app = new App();
+            app.DispatcherUnhandledException += App_DispatcherUnhandledException;
             app.InitializeComponent();
 
             var applicationMonitorScope = new ApplicationMonitorScope(new WpfNotificationContext(app.Dispatcher));
@@ -24,6 +30,25 @@ namespace JiraToolkit
             {
                 applicationMonitorScope.Dispose().GetAwaiter().GetResult();
             }
+        }
+
+        static void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            logger.Fatal("An exception which terminates the application occurred", e.Exception);
+
+            var result = MessageBox.Show(
+                $"Do you want to create a bug report?{Environment.NewLine}This will open the log file and a browser to create a bug ticket.",
+                "An unknown error occurred. The application shuts down",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Error);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Process.Start($"{AppDomain.CurrentDomain.BaseDirectory}/jtk.log");
+                Process.Start("https://github.com/Tymoniden/JiraToolkit/issues/new");
+            }
+
+            Environment.Exit(-1);
         }
     }
 }
